3ce946690775d73bf1ab4439c531c729bd7b5f12
KhoiDOO <khoido8899@gmail.com>
2023-03-12 14:42:39 +0700

    update content

--- contract/sub_modules/marker_content.py
+++ contract/sub_modules/marker_content.py
@@ -1,7 +1,17 @@
-markers = {
+import os
 
-}
+source_txt = os.getcwd() + "/content/ds_cty.txt"
+with open(source_txt, "r", encoding="utf-8") as f:
+    lines = f.readlines()
+    cty_lst = [x[:-1] for x in lines if "CÔNG TY" in x and "Mã số thuế" not in x]
+    dc_lst = [" ".join(x[:-1].split(":")[-1].replace(",", " ").split(" ")) for x in lines if "Địa chỉ" in x]
+
 
 content = {
-    "name" : []
+    "company_add" : dc_lst,
+    "company_name" : cty_lst
 }
+
+if __name__ == "__main__":
+    pass
+    # print(content["company_name"])
