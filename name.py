6c1098e87181a583295642b0062d7db046b5602d
NguyenHongSon1103 <son0915876256@gmail.com.vn>
2023-03-12 15:29:18 +0700

    fix module bug

--- name.py
+++ name.py
@@ -14,11 +14,9 @@ class Buyer(Module):
 
     def __call__(self, submodules: list):
         for sub_module in submodules:
-
-            self.__paste__(submodule=sub_module, position=(100, 100))
+            self.__paste__(submodule=sub_module, position=(1200, 100))
 
 class Contract_Name(SubModule):
-
     def __init__(self, shape=None, canvas=None, marker_prob=0.5, marker_font: ImageFont.truetype = None, 
                  content_font: ImageFont.truetype = None, markers=None, content=None, label=None):
         super().__init__(shape, canvas, marker_prob, marker_font, content_font, markers, content, label)    
@@ -32,16 +30,17 @@ if __name__ == "__main__":
     # print(test.fields)
 
     # for point in test.fields:
-    #     start_point = (point["xmin"], point['ymin'])
-    #     end_point = (point["xmax"], point['ymax'])
-    #     test.canvas = cv2.rectangle(test.canvas, start_point, end_point, (0, 0, 255), thickness = 2)
+    #     x1, y1, x2, y2 = point['box']
+    #     test.canvas = cv2.rectangle(test.canvas, (x1, y1), (x2, y2), (0, 0, 255), thickness = 2)
 
     # cv.imshow("test", test.canvas)
     # cv.waitKey(0)
 
     buyer_test = Buyer(shape = (3000, 3000))
-    print(buyer_test.get_shape())
     buyer_test([test])
+    for point in buyer_test.get_fields():
+        x1, y1, x2, y2 = point['box']
 
+        buyer_test.canvas = cv2.rectangle(buyer_test.canvas, (x1, y1), (x2, y2), (0, 0, 255), thickness = 2)
     cv.imshow("test", buyer_test.canvas)
-    cv.waitKey(0)    
+    key = cv.waitKey(0)    
